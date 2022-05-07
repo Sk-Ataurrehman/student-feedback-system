d228da030e9a4ac1922fa37184f6292973d052b1
unknown <shaikhataurrehman0@gmail.com>
2022-05-07 20:35:09 +0530

    Final Commit

--- app.js
+++ app.js
@@ -89,7 +89,6 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  // render the error page
   console.log(err);
   res.status(err.status || 500);
   res.render("error", { message: err.message, error: err });
